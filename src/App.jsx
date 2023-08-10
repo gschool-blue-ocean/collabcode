a5179845553b8ed5979a8de43497ef4b1397c30d
Bao Tran <Baottran21@outlook.com>
2023-08-10 15:34:30 -0500

    Restructured for other pages and currently working on teacher page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage/LandingPage';
+
+//Mock Components
+//Teacher
+import TeacherAdminPage from './components/TeacherAdminPage/TeacherAdminPage';
+//Student
 
 function App() {
   return (
     <>
       <LandingPage />
+      <TeacherAdminPage />
     </>
   );
 }
---
+++ src/components/TeacherAdminPage/TeacherAdminPage.jsx
@@ -0,0 +1,11 @@
+import Header from '../Header';
+
+const TeacherAdminPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default TeacherAdminPage;
