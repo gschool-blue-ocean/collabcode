08f1a0644ff9d1959db952b42d5722dadf9b6f2d
Bao Tran <Baottran21@outlook.com>
2023-08-10 10:40:19 -0500

    Built the layout for how the landing page will be structured

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+const LandingPage = () => {
+  return (
+    <>
+      <div
+        id="landing-page-container"
+        className="w-full h-[100vh] flex flex-col items-center justify-center"
+      >
+        <h1>This will be the header component</h1>
+        <h1>This will be the Information component</h1>
+        <h1>This will be the runtime component</h1>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
