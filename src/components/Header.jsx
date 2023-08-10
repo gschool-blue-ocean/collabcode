d593f99b6d1904a079304bb7d0e2304cc5e9126a
Bao Tran <Baottran21@outlook.com>
2023-08-10 12:20:08 -0500

    Built Shells for each component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ const Header = () => {
     <>
       <div
         id="Navbar"
-        className="flex h-[10vh] w-full items-center justify-between"
+        className="flex h-[10vh] w-full items-center justify-between mt-[1rem]"
       >
-        <h1 className="text-[3rem]">CollabCode</h1>
-        <div id="navigation" className="flex mr-[5rem]">
-          <h1>Home</h1>
-          <h1>Teachers</h1>
-          <h1>Students</h1>
+        <h1 className="text-[3rem] ml-[5rem]">CollabCode</h1>
+        <div id="navigation-container" className="mr-[5rem]">
+          <div className="w-[10rem] flex justify-between">
+            <h1>Home</h1>
+            <h1>Login/SignUp</h1>
+          </div>
         </div>
       </div>
     </>
