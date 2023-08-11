a0f3e17c80440ef9f5e1d67053d68d0118a6214e
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-08-11 13:18:35 -0700

    added query for ta_id and st_id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import pg from 'pg';
 import cors from 'cors';
-import { param, body, validationResult } from 'express-validator';
+import { param, body, query, validationResult } from 'express-validator';
 
 // initialize app by invoking express
 const app = express();
@@ -595,23 +595,74 @@ app.delete(
 
 /*----- 'interviews' table routes -----*/
 
-// GET ALL - secured by not reading request object
+// GET ALL - secured by sanitizing query
 app.get(
     '/interviews',
+    query('ta_id').blacklist(';').escape(),
+    query('st_id').blacklist(';').escape(),
     async (req, res) => {
-        try {
-            const results = await pool.query(
-                'SELECT * FROM interviews;'
-            );
-            if (results.rowCount < 1) {
-                res.status(404).send('Resource not found'); return;
-            } else {
-                res.status(200).json(results.rows); return;
+        // validation result
+        if (!validationResult(req).isEmpty) {
+            res.status(400).send(
+                "Validator caught the following error(s): " +
+                validationResult(req).array()
+            ); return;
+        }
+
+        // destruct teacher id and student id from query
+        const { ta_id, st_id } = req.query;
+        // if a teacher id exists, but not a student id, GET ALL interviews by teacher id
+        if (ta_id) {
+            try {
+                const results = await pool.query(
+                    'SELECT * FROM interviews WHERE ta_id = $1;',
+                    [ta_id]
+                );
+                if (results.rowCount < 1) {
+                    res.status(404).send('Resource not found'); return;
+                } else {
+                    res.status(200).json(results.rows); return;
+                }
+            }
+            catch (error) {
+                console.error(error.message);
+                res.status(500).send('Server caught the following error: ' + error.message); return;
             }
         }
-        catch (error) {
-            console.error(error.message);
-            res.status(500).send('Server caught the following error: ' + error.message); return;
+        // if a student id exists, but not a teacher id, GET ALL interviews by student id
+        else if (st_id) {
+            try {
+                const results = await pool.query(
+                    'SELECT * FROM interviews WHERE st_id = $1;',
+                    [st_id]
+                );
+                if (results.rowCount < 1) {
+                    res.status(404).send('Resource not found'); return;
+                } else {
+                    res.status(200).json(results.rows); return;
+                }
+            }
+            catch (error) {
+                console.error(error.message);
+                res.status(500).send('Server caught the following error: ' + error.message); return;
+            }
+        }
+        // if neither exist, GET ALL normally
+        else {
+            try {
+                const results = await pool.query(
+                    'SELECT * FROM interviews;'
+                );
+                if (results.rowCount < 1) {
+                    res.status(404).send('Resource not found'); return;
+                } else {
+                    res.status(200).json(results.rows); return;
+                }
+            }
+            catch (error) {
+                console.error(error.message);
+                res.status(500).send('Server caught the following error: ' + error.message); return;
+            }
         }
     }
 );
