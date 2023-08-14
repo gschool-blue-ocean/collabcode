0e4dd388756f65abb9c54f53ba3834b2ca0c3ccb
Bao Tran <Baottran21@outlook.com>
2023-08-14 18:10:11 -0500

    Merging with Main

--- db/teacherSeed.sql
+++
@@ -1,6 +0,0 @@
--- Seeding a teacher example
-INSERT INTO teachers (ta_email, ta_password, ta_name) 
-VALUES 
-('teacher@hotmail.com', '123imaboss', 'Frank J Fletcher'),
-('GarrettRoss@gmail.com', 'garrett', 'Adam Ross'),
-('DavidHooper@yahoo.com', 'david', 'David Hooper');
