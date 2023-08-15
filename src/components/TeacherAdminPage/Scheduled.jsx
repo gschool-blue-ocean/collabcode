0e5802bd60ed057cd07ff2235fe09727349668db
Bao Tran <Baottran21@outlook.com>
2023-08-15 11:18:23 -0500

    Finished the preliminary Calendar Component

--- src/components/TeacherAdminPage/Scheduled.jsx
+++ src/components/TeacherAdminPage/Scheduled.jsx
@@ -7,13 +7,17 @@ const Scheduled = () => {
     <>
       <div
         id="calendar-app"
-        className="w-[40vw] h-[40vh] flex flex-col justify-center items-center"
+        className="w-[40vw] h-[45vh] flex flex-col justify-center items-center border-solid border-4 border-indigo-400"
       >
-        <h1 id="header">Scheduled Appointments</h1>
+        <h1 id="header" className="text-[2rem]">
+          Scheduled Appointments
+        </h1>
         <div id="calendar-container" className="flex justify-center">
           <Calendar onChange={setDate} value={date} />
         </div>
-        <div className="text-center">Selected date: {date.toDateString()}</div>
+        <div className="text-[1.5rem] ">
+          Selected date: {date.toDateString()}
+        </div>
       </div>
     </>
   );
