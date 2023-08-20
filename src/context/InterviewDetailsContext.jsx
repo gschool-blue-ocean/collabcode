cb1881d8fff3725f7c862d24262eb075a90477ec
Bao Tran <Baottran21@outlook.com>
2023-08-20 15:26:46 -0500

    Styling of the teacher component

--- src/context/InterviewDetailsContext.jsx
+++ src/context/InterviewDetailsContext.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable react/prop-types */
-import {  createContext, useRef } from "react";
+import { createContext, useRef } from "react";
 
 const interviewDetails = [
-    {
-        date: '2023-08-10',
-        time: '11:00AM',
-        instructor: 'Phil'
-    }
-]
+  {
+    date: "2023-08-10",
+    time: "11:00AM",
+    instructor: "Phil",
+  },
+];
 
 const InterviewDetailsContext = createContext();
 
 export const InterviewDetailsProvider = ({ children }) => {
-    let detailsInfo = useRef(interviewDetails);
-    detailsInfo = detailsInfo.current
+  let detailsInfo = useRef(interviewDetails);
+  detailsInfo = detailsInfo.current;
 
-    return (
-        <InterviewDetailsContext.Provider value={{ detailsInfo }}>
-            {children}
-        </InterviewDetailsContext.Provider>
-    )
-}
+  return (
+    <InterviewDetailsContext.Provider value={{ detailsInfo }}>
+      {children}
+    </InterviewDetailsContext.Provider>
+  );
+};
 
-export default InterviewDetailsContext
+export default InterviewDetailsContext;
